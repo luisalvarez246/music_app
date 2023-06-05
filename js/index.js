36c33dbb5d97a1eaf947e0b549b632b22ac3fae9
Javier <132447183+Toyos90@users.noreply.github.com>
2023-06-05 20:08:19 +0200

    index.js

--- js/index.js
+++ js/index.js
@@ -1,19 +1,6 @@
-/*const nav = document.querySelector("#nav");
-const open = document.querySelector("#open");
-const close = document.querySelector("#close");
-
-open.addEventlistener("click", () => {
-    nav.classList.add("visible");
-})
-
-close.addEventlistener("click", () =>{
-    nav.classList.remove("visible")
-})
-*/
-
 toggle.onclick = () => {
-    items.classList.toggle("open");
-    items.classList.toggle("close");
+  items.classList.toggle("open");
+  items.classList.toggle("close");
 }
 
 function includeHTML() {
@@ -43,4 +30,3 @@ function includeHTML() {
       }
     }
   };
-
