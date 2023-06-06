27fddf35553d2f3061e73129c1817fc4e911e201
Lualvare <lualvare@Lualvares-MacBook.local>
2023-06-06 16:32:26 +0200

    Solved problems from jungle/selection merging

--- js/selection.js
+++
@@ -1,9 +0,0 @@
-import { goToPage } from "./index.js";
-
-document.getElementById("drumButton").addEventListener("click", (event) => {
-    goToPage("./../html/drums.html")
-})
-
-document.getElementById("kalimbaButton").addEventListener("click", (event) => {
-    goToPage("./../html/kalimba.html")
-})
